5e8657b5905bd62c3b030eb3973330388f1e390c
Ivan <75654991+ibarraz5@users.noreply.github.com>
2022-05-29 16:14:53 -0700

    Update WebServer.java

--- WebServer/src/main/java/funHttpServer/WebServer.java
+++ WebServer/src/main/java/funHttpServer/WebServer.java
@@ -250,8 +250,8 @@ class WebServer {
           query = splitQuery(request.replace("image?", ""));
 
           // extract required fields from parameters
-          String path = String(query.get("path"));
-          String path2 = String(query.get("path2"));
+          String path = query.get("path");
+          String path2 = String.(query.get("path2"));
 
 
           try{ 
