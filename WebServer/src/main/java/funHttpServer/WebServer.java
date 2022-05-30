be2ba18b29eb76590b123fef98b12434d6f04cf0
Ivan <75654991+ibarraz5@users.noreply.github.com>
2022-05-29 21:22:36 -0700

    Update WebServer.java

--- WebServer/src/main/java/funHttpServer/WebServer.java
+++ WebServer/src/main/java/funHttpServer/WebServer.java
@@ -8,7 +8,8 @@ You can also do some other simple GET requests:
 4) /multiply?num1=3&num2=4 multiplies the two inputs and responses with the result
 5) /github?query=users/amehlhase316/repos (or other GitHub repo owners) will lead to receiving
    JSON which will for now only be printed in the console. See the todo below
-6) /survey generates a survey form that can be filled out and submitted
+6) /survey generates a survey form that can be filled out and submitted.
+7) /invoice makes a sample invoice come up with the asu logo and billing information.
 The reading of the request is done "manually", meaning no library that helps making things a 
 little easier is used. This is done so you see exactly how to pars the request and 
 write a response back
